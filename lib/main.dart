bff8f168c3d7331dbbdb9f34b31fbf552e0f84b7
rishab <rishabaggarwal@outlook.in>
2024-06-10 00:58:32 +0530

    Structured Properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,20 @@
-import 'package:flutter/material.dart';
+import 'package:dhi/pages/home.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import 'package:dhi/hero_section.dart';
-import 'package:dhi/knowledge_snacks.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        title: const Text('Dhi'),
-      ),
-
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              HeroSection(),
-              Divider(thickness: 1, color: Color(0xFFCAC4D0)),
-              KnowledgeSnacks()
-            ],
-          ),
-        ),
-
-      ),
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-    );
-  }
-}
+import 'package:flutter/material.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
