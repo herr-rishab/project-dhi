518e943db6419a3e20378b4b899d6c004ffb970d
rishab <rishabaggarwal@outlook.in>
2024-06-10 00:58:24 +0530

    Structured Properly

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dhi/main.dart';
+import 'package:dhi/pages/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
